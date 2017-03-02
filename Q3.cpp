35bc1cff9764e8d0b91f3817792c7e01bd14bcc9
lucasnnobrega <lucasnnobrega@users.noreply.github.com>
2017-03-02 13:42:11 -0300

    Estrutura feita, falta o código

---
+++ Q3.cpp
@@ -0,0 +1,44 @@
+/*******
+ * Nome:Lucas Nogueira Nobrega
+ *      Luciano Vieira Da Silva Júnior
+ *
+ * Data:02-03-2017
+ *
+ * Resumo do código:
+ *           Escreva um programa em C++ que simula o lançamento de um dado n vezes e imprime
+ *           o percentual de surgimento de cada face do dado. O valor n é introduzido pelo
+ *           usuário, sendo que 0 encerra o programa.
+ ********
+ */
+
+/*
+    Tem:
+        Do while
+        simula n de jogadas
+        Criar uma função com:
+            imprime o percentual valor/n de jogadas
+        Condição 0 = end
+*/
+
+
+
+
+
+
+//Bibliotecas
+#include <iostream>
+//#include <stdio.h>      /* printf, NULL */
+#include <stdlib.h>     /* srand, rand */
+//#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    cout << rand() % 50;
+
+
+    return 0;
+}
+
