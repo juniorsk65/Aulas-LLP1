afaa9bf6a57468fbf916f94881758c7950937f2a
Luciano Jr <junior_sk65@hotmail.com>
2017-03-06 21:20:29 -0300

    Modificações do codigo, necessario revisão!

--- Q1.cpp
+++ Q1.cpp
@@ -5,6 +5,28 @@ using namespace std;
 
 int main()
 {
-    cout << "testando" << endl;
-    
-}
+    int num, aux, passou, maior, i, cont = 0, lista[] = {0};
+    cout << "Vamos achar o maior número, quando decidir parar basta digitar 0. \n" << endl;
+    cout << "Digite um numero" <<endl;
+    cin >> num;
+
+    while(num != 0){
+        lista[cont] = num;
+        cout << "Digite outro numero" << endl;
+        cin >> num;
+        cont++;
+    };//Fim  while
+    for(passou = 1; passou < cont; passou++)
+    {
+        for(i = 0; i < cont - 1; i++)
+        {
+            if(lista[i] < lista[i+1])
+            {
+                aux = lista[i];
+                lista[i] = lista[i + 1];
+                lista[i + 1] = aux;
+            }
+        }//Fim contador array
+    }//Fim Passou
+    cout << "\nO maior numero digitado foi: " << lista[0] << endl;
+} //Fim Main
