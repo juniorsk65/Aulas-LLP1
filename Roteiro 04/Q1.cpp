be38ee54e93d6e7dfc0839290a11783fdb7a6d48
juniorsk65 <junior_sk65@hotmail.com>
2017-03-07 13:44:45 -0300

    Comentários adicionais.

--- Roteiro 04/Q1.cpp
+++ Roteiro 04/Q1.cpp
@@ -9,17 +9,19 @@ int main()
     cout << "Vamos achar o maior número, quando decidir parar basta digitar 0. \n" << endl;
     cout << "Digite um numero" <<endl;
     cin >> num;
-
+    
+    //Enquanto o numero inserido for diferente de 0, continue
     while(num != 0){
         lista[cont] = num;
         cout << "Digite outro numero" << endl;
         cin >> num;
         cont++;
     };//Fim  while
+    //Metodo da bolha de ordenação.
     for(passou = 1; passou < cont; passou++)
     {
         for(i = 0; i < cont - 1; i++)
-        {
+        {   //Verificando qual é maior
             if(lista[i] < lista[i+1])
             {
                 aux = lista[i];
