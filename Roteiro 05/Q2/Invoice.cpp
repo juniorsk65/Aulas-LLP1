b8c5fdee4e871831a15d863b10549c6ca1c2e377
Luciano Jr <junior_sk65@hotmail.com>
2017-03-09 06:35:39 -0300

    Implementação da classe testada com sucesso.

--- Roteiro 05/Q2/Invoice.cpp
+++ Roteiro 05/Q2/Invoice.cpp
@@ -1,5 +1,57 @@
 #include "Invoice.h"
+#include <iostream>
 
-Invoice::Invoice(){
-    return 0;
+using namespace std;
+//Implementação do metodo construtor e passagem dos parametros para a criação do objeto;
+Invoice::Invoice(int numero_ct, int quantidade_ct, string descricao_ct, float preco_ct){
+        setNumero(numero_ct);
+        setQuantidade(quantidade_ct);
+        setDescricao(descricao_ct);
+        setPreco(preco_ct);
+}
+
+int Invoice::getNumero(){
+        return numero;
+}
+
+int Invoice::getQuantidade(){
+        return quantidade;
+}
+
+string Invoice::getDescricao(){
+        return descricao;
+}
+
+float Invoice::getPreco(){
+        return preco;
+}
+
+void Invoice::setNumero(int num){
+        numero = num;
+}
+
+void Invoice::setQuantidade(int qtd){
+        quantidade = qtd;
+}
+
+void Invoice::setDescricao(string desc){
+        descricao = desc;
+}
+
+void Invoice::setPreco(float pre){
+        preco = pre;
+}
+int main(){
+        //Testando a implementação da classe e criando um objeto chamado fatura
+        Invoice fatura( 1, 2,"teste de string2");
+
+        int n_o = fatura.getNumero();
+        int q_o = fatura.getQuantidade();
+        string d_o = fatura.getDescricao();
+        float p_o = fatura.getPreco();
+        
+        cout << n_o << endl;
+        cout << q_o << endl;
+        cout << d_o << endl;
+        cout << p_o << endl;
 }
--- Roteiro 05/Q2/Invoice.h
+++ Roteiro 05/Q2/Invoice.h
@@ -1,13 +1,31 @@
-#ifdef INVOICE_H
-#endif INVOICE_H
+#ifndef INVOICE_H
+#define INVOICE_H
+#include <string> //Separando como .h é obrigatorio declarar a biblioteca que não é padrao da linguagem aqui
+
+using namespace std;
 
 class Invoice{
     private: //Definição dos atributos
-    int numero, quantidade;
-    char descricao;
-    float preco;
-
-    //Metodo Construtor
-    Invoice();
-    
+            int numero, quantidade;
+            string descricao; //Quando utilizar uma string, é obrigatorio colocar o namespace no escopo.
+            float preco;
+    public:
+           
+            //Set e Get Numero
+            void setNumero(int num);
+            int getNumero();
+            //Set e Get Quantidade
+            void setQuantidade(int qtd);   
+            int getQuantidade();
+            //Set e Get Descrição
+            void setDescricao(string desc);
+            string getDescricao();
+            //Set e Get Preço
+            void setPreco(float pre);
+            float getPreco();
+            //Calculo do valor
+            float getInvoiceAmount();
+             //Metodo Construtor
+            Invoice(int numero_ct = 0, int quantidade_ct = 0, string descricao_ct = "", float preco_ct = 0.0);
 };
+#endif //INVOICE_H
