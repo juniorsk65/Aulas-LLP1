6fd8b13a0768804ca493bd7ce9f8ce0f6896821d
Luciano Jr <junior_sk65@hotmail.com>
2017-03-16 09:50:15 -0300

    Ultimas alterações para submissão

--- Roteiro 05/Q6/Pagamento.cpp
+++ Roteiro 05/Q6/Pagamento.cpp
@@ -20,7 +20,7 @@ ControleDePagamento::ControleDePagamento(){
 double ControleDePagamento::calculaTotalDePagamentos()
 {
     double valor;
-    for(int i = 0; i < 30; i++ ){
+    for(int i = 0; i < nPagamentos; i++ ){
         valor += pagamentos[i].getValorPagamento();
     }
     return valor;
@@ -30,21 +30,21 @@ void ControleDePagamento::setPg(Pagamento pg[], int n){
     for(int i = 0; i < n; i++){
         pagamentos[j++] = pg[i];
     }
-    nPagamentos += n;
+    nPagamentos = n;
 }
 
 bool ControleDePagamento::existePagamentoParaFuncionario(string nomeFuncionario){
-    for(int i = 0; i < 30; i++){
+    for(int i = 0; i < nPagamentos; i++){
         if(pagamentos[i].getNomeDoFuncionario() == nomeFuncionario)
         {
-            cout << "Sim, existe pagamento para: " << nomeFuncionario << endl;
+            cout << "Sim, existe pagamento para: " << nomeFuncionario << "// Valor = R$" << pagamentos[i].getValorPagamento() << endl;
             return true;
         }
     }
 }
 
 void ControleDePagamento::listarPagamentos(){
-    for(int i = 0 ; i < 30; i++){
+    for(int i = 0 ; i < nPagamentos; i++){
         cout << pagamentos[i].getNomeDoFuncionario() << "  " << pagamentos[i].getValorPagamento() << endl;
     }
 }
@@ -52,8 +52,7 @@ void ControleDePagamento::listarPagamentos(){
 int main()
 {
     ControleDePagamento funcionario;
-    int controle, i=0;
-    double valor;
+    int controle;
     string nomeF;
     //pg[] tambem é um array do tipo Pagamento, local para depois colocar em pagamentos com setPg
     Pagamento pg[30];
@@ -62,25 +61,16 @@ int main()
     pg[2] = Pagamento(20.00, "Lucas");
     pg[3] = Pagamento(33.05, "Thiago");
     //sizeof(pg)/sizeof(Pagamento) - pega a quantidade de objetos que foram inseridos.
-    funcionario.setPg(pg, sizeof(pg)/sizeof(Pagamento));
-    int n = sizeof(pg)/sizeof(Pagamento);
-    /*
-    cout << funcionario.pagamentos[1].getNomeDoFuncionario() << endl;
-    cout << funcionario.pagamentos[1].getValorPagamento() << endl;
-
-    double saldo = 0;
-    saldo = funcionario.calculaTotalDePagamentos();
-    cout << "saldo Devedor = R$" << saldo << endl;
-    cout << funcionario.existePagamentoParaFuncionario("Luciano") << endl;
-   */
-
+    funcionario.setPg(pg, 4);
+    
     cout << "********** MENU **********" << endl;
-    cout << "1 - Cadastrar Pagamento\n" << "2 - Consultar Pagamentos\n" << "3 - Listar Pagamentos" << endl;
+    cout << "1 - Cadastrar Pagamento\n" << "2 - Consultar Pagamentos\n" << "3 - Listar Pagamentos\n" << "4 - Total de Pagamentos" << endl;
     cin >> controle;
 
     switch(controle){
         case 1:
         //Cadastrar Pagamento
+        cout << "Desculpe, função não implementada!!" << endl;
         break;
         case 2:
         //Consultar pagamentos de funcionario
@@ -91,6 +81,9 @@ int main()
         case 3:
         // Listar Pagamentos
         funcionario.listarPagamentos();
+        break;
+        case 4:
+        cout << "Total de pagamentos = R$" << funcionario.calculaTotalDePagamentos() << endl;
         break;
     }
 }
