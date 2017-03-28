82f474f8aa50e55a63ea78501615411f64560ac0
Lucas N Nobrega <lucasnnobrega@gmail.com>
2017-03-28 09:36:59 -0300

    Classe relogio esta funcionando

--- Roteiro 06/Q1/Relogio.cpp
+++ Roteiro 06/Q1/Relogio.cpp
@@ -7,8 +7,8 @@ int Relogio::getHoras(){return horas;};
 int Relogio::getMinutos(){return minutos;};
 int Relogio::getSegundos(){return segundos;};
 
-bool Relogio::setHorario(int h;  int m; int s;){
-    if(0 < h && h < 23 && 0 < m && m < 59 && 0 < s && s < 59)
+bool Relogio::setHorario(int h,  int m, int s){
+    if(0 <= h && h <= 23 && 0 <= m && m <= 59 && 0 <= s && s <= 59)
     {
         horas = h;
         minutos = m;
@@ -19,16 +19,31 @@ bool Relogio::setHorario(int h;  int m; int s;){
 }
 
 void Relogio::avancarHorario(){
-    segundo++;
-    if(segundo == 60){
-        segundo = 0;
-        minuto ++;
-        if (minuto == 60){
-            minuto = 0;
-            hora++;
-            if(hora == 24){
-                hora = 0;
+    segundos++;
+    if(segundos == 60){
+        segundos = 0;
+        minutos ++;
+        if (minutos == 60){
+            minutos = 0;
+            horas++;
+            if(horas == 24){
+                horas = 0;
             }
         }
     }
+}
+
+int main(){
+    Relogio analog;
+    analog.setHorario(23, 59, 59);
+
+    cout << analog.getHoras();
+    cout << analog.getMinutos();
+    cout << analog.getSegundos() << endl;
+    analog.avancarHorario();
+    cout << analog.getHoras();
+    cout << analog.getMinutos();
+    cout << analog.getSegundos();
+
+
 }
