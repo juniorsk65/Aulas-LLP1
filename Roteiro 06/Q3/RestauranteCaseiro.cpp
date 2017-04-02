ff66c99ee76e12d84d7219a974444ce3ae5dac80
lucasnnobrega <lucasnnobrega@users.noreply.github.com>
2017-04-01 22:16:03 -0300

    Comentado?

--- Roteiro 06/Q3/RestauranteCaseiro.cpp
+++ Roteiro 06/Q3/RestauranteCaseiro.cpp
@@ -42,6 +42,7 @@ double RestauranteCaseiro::calculaTotalRestaurante(){
 int main()
 {
     RestauranteCaseiro restaurante;
+    //Cardápio
     Pedido pedi[10];
     pedi[0] = Pedido(1231, 2, 2.00, "Arroz");
     pedi[1] = Pedido(2536, 3, 3.00, "Feijao");
@@ -49,11 +50,11 @@ int main()
     pedi[3] = Pedido(9988,1, 40.00, "Lagosta");
     pedi[4] = Pedido(5533, 1, 18.00, "Coca");
     pedi[5] = Pedido(4142, 2, 50.00, "Picanha");
+    //Fim Cardápio
 
-
-    restaurante.mesa[1].adicionaAoPedido(pedi[5]);
-    restaurante.mesa[2].adicionaAoPedido(pedi[2]);
-    restaurante.mesa[1].adicionaAoPedido(pedi[5]);
+    restaurante.mesa[1].adicionaAoPedido(pedi[5]);//Picanha na mesa 1
+    restaurante.mesa[2].adicionaAoPedido(pedi[2]);//Salada na mesa 2
+    restaurante.mesa[1].adicionaAoPedido(pedi[5]);//Picanha na mesa 1
     
     cout.precision(2);//Precisa usar o fixed antes
     cout << restaurante.mesa[1].pedido[0].getDescricao() << endl;
@@ -63,7 +64,9 @@ int main()
     cout << fixed << restaurante.mesa[1].calculaTotal() << endl;
     restaurante.mesa[1].zeraPedidos(0);
 
+    //Na Mesa 1 adiciona o pedido numero 5
     restaurante.adicionaAoPedido(1,pedi[5]);
+
     cout << restaurante.calculaTotalRestaurante() << endl;
     return 0;
 }
