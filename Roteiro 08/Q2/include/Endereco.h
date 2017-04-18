b83fcc845dffcbf3c54bb35e0cef685aa2c4cc60
Luciano Jr <junior_sk65@hotmail.com>
2017-04-18 06:47:03 -0300

    Iniciando classe Endereco, metodos set e get finalizados

---
+++ Roteiro 08/Q2/include/Endereco.h
@@ -0,0 +1,30 @@
+#ifndef ENDERECO_H
+#define ENDERECO_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Endereco {
+    private:
+        string logradouro, bairro, cidade, cep;
+        int numero;
+    public:
+        string getLogradouro();
+        string getBairro();
+        string getCidade();
+        string getCep();
+        int getNumero();
+
+        void setLogradouro(string logradouro_);
+        void setBairro(string bairro_);
+        void setCidade(string cidade_);
+        void setCep(string cep_);
+        void setNumero(int numero_);
+
+        Endereco(string ct_logradouro, string ct_bairro, string ct_cidade, 
+                string ct_cep, int ct_numero);
+};
+
+
+#endif //ENDERECO_H
---
+++ Roteiro 08/Q2/src/Endereco.cpp
@@ -0,0 +1,32 @@
+#include "../include/Endereco.h"
+
+Endereco::Endereco(string ct_logradouro, string ct_bairro, string ct_cidade,
+                   string ct_cep, int ct_numero)
+{
+    setLogradouro(ct_logradouro);
+    setBairro(ct_bairro);
+    setCidade(ct_cidade);
+    setCep(ct_cep);
+    setNumero(ct_numero);
+}
+string Endereco::getLogradouro(){return logradouro;}
+string Endereco::getBairro(){return bairro;}
+string Endereco::getCidade(){return cidade;}
+string Endereco::getCep(){return cep;}
+int Endereco::getNumero(){return numero;}
+
+void Endereco::setLogradouro(string logradouro_){
+    logradouro = logradouro_;
+}
+void Endereco::setBairro(string bairro_){
+    bairro = bairro_;
+}
+void Endereco::setCidade(string cidade_){
+    cidade = cidade_;
+}
+void Endereco::setCep(string cep_){
+    cep = cep_;
+}
+void Endereco::setNumero(int numero_){
+    numero = numero_;
+}
